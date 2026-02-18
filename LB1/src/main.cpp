b55b55b2a34a82f34beb945276dec92d95430267
Larionov-VA <vlad.larionov.80@mail.ru>
2026-02-18 17:19:39 +0300

    lb1: adds

--- LB1/src/main.cpp
+++ LB1/src/main.cpp
@@ -1,6 +1,7 @@
 // Возможно стоит убрать исключения
 #include <stdexcept>
 
+#include <climits>
 #include <iostream>
 #include <string>
 #include <bitset>
@@ -36,6 +37,13 @@ void printLineAsBinary(u_int64_t line, size_t gridSide) {
 }
 
 
+void addSquare(Square& sourceSquare, u_int64_t* destinationSquare, size_t squareSide) {
+    for (int i = 0; i < squareSide; ++i) {
+        sourceSquare.binaryGridOfSquare[i] |= destinationSquare[i];
+    }
+}
+
+
 u_int64_t* buildSquareMask(size_t gridSide, size_t squareSide, Point squareRigthUpCoordinates) {
     if (!isPointCorrect(gridSide, squareSide, squareRigthUpCoordinates)) {
         throw std::runtime_error(
@@ -53,9 +61,6 @@ u_int64_t* buildSquareMask(size_t gridSide, size_t squareSide, Point squareRigth
     for (size_t i = 0; i < squareSide; ++i) {
         gridWithSquare[squareRigthUpCoordinates.y + i] |= lineOfSquare;
     }
-    for (size_t i = 0; i < gridSide; ++i) {
-        printLineAsBinary(gridWithSquare[i], gridSide);
-    }
     return gridWithSquare;
 }
 
@@ -69,10 +74,34 @@ int main() {
     long long squareSide;
     std::cin >> squareSide;
     if (squareSide < 2) return EXIT_FAILURE;
+    Square squareInfo = {
+        0,
+        INT_MAX,
+        static_cast<size_t>(squareSide),
+        buildSquareMask(static_cast<size_t>(squareSide), 0, {0,0})};
     if (!STEPIK) {
-
+        int numberOfSquares;
+        std::cin >> numberOfSquares;
+        Point p;
+        long long currentSuareSide;
+        for (int i = 0; i < numberOfSquares; ++i) {
+            std::cin >> p.x >> p.y >> currentSuareSide;
+            if (currentSuareSide < 0) return EXIT_FAILURE;
+            addSquare(
+                squareInfo,
+                buildSquareMask(
+                    static_cast<size_t>(squareSide),
+                    static_cast<size_t>(currentSuareSide),
+                    {p.x,p.y}
+                ),
+                squareSide
+            );
+            for (size_t i = 0; i < static_cast<size_t>(squareSide); ++i) {
+                printLineAsBinary(squareInfo.binaryGridOfSquare[i], squareSide);
+            }
+        }
     }
-    buildSquareMask(static_cast<size_t>(squareSide), 1, {4,1});
-    findMinimalNumberOfParts();
+
+    // findMinimalNumberOfParts();
     return EXIT_SUCCESS;
 }
