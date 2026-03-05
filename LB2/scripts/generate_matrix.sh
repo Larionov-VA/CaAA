d97e2ea3a7699f658184511d8c3ae9a008ec40fd
Larionov-VA <vlad.larionov.80@mail.ru>
2026-03-05 11:12:42 +0300

    lb2: fix scripts

--- LB2/scripts/generate_matrix.sh
+++ LB2/scripts/generate_matrix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p ./build
-mkdir -p ./build/matrices
-mkdir -p ./files
+mkdir -p ./files/matrices
 g++ ./utilities/generate_matrix.cpp -o ./build/generate_matrix
 ./build/generate_matrix
--- LB2/scripts/get_MST.sh
+++ LB2/scripts/get_MST.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p ./build
-mkdir -p ./files
 g++ ./src/main.cpp -o ./build/main
 ./build/main < ./files/matrices/example.txt
 dot -Tsvg ./files/graph.dot -o ./files/graph.svg
