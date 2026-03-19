a857c5484195865dd8c849c80f90849b5a50777d
Larionov-VA <vlad.larionov.80@mail.ru>
2026-03-19 18:59:09 +0300

    lb2: ok

--- LB2/src/main.cpp
+++ LB2/src/main.cpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <algorithm>
 #include <limits>
+
 #define GRAPH_FILENAME "./files/graph.dot"
 #define MST_FILENAME "./files/MST.dot"
 
@@ -20,21 +21,33 @@ struct edge {
 using edgesStack = std::vector<edge>;
 using matrix = std::vector<std::vector<int>>;
 
-struct partialSolution {
-    edgesStack sortedAvailableEdges;
-    edgesStack includedEdges;
-    int vertexNumber;
-    int currentResult = 0;
-    int bestResult = std::numeric_limits<int>::max();
+struct DSU {
+    std::vector<int> parent, rank;
+    DSU(int n) : parent(n), rank(n, 0) {
+        for (int i = 0; i < n; ++i)
+            parent[i] = i;
+    }
+    int find(int v) {
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
+    }
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return false;
+        if (rank[a] < rank[b]) std::swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b]) rank[a]++;
+        return true;
+    }
 };
 
-
 matrix getMatrixFromFile() {
-    int squareMatrixSide;
-    std::cin >> squareMatrixSide;
-    matrix M(squareMatrixSide, std::vector<int>(squareMatrixSide));
-    for (int i = 0; i < squareMatrixSide; ++i) {
-        for (int j = 0; j < squareMatrixSide; ++j) {
+    int n;
+    std::cin >> n;
+    matrix M(n, std::vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
             std::cin >> M[i][j];
         }
     }
@@ -50,44 +63,38 @@ void matrixToDot(const matrix& M, const std::string& filename) {
     dotFile << "    node [shape=circle];\n\n";
     for (size_t i = 0; i < M.size(); ++i) {
         for (size_t j = i + 1; j < M.size(); ++j) {
-            if(M[i][j]) {
+            if (M[i][j]) {
                 dotFile << "    " << i << " -- " << j
-                        << " [label=\"" << M[i][j] << "\" color=\"green\" penwidth=\"2\"];\n";
+                        << " [label=\"" << M[i][j]
+                        << "\" color=\"green\" penwidth=\"2\"];\n";
             }
         }
     }
     dotFile << "}\n";
 }
 
-void printMatrix(const matrix& M) {
-    for (const auto& row : M) {
-        for (int val : row) {
-            std::cout << val << ' ';
-        }
-        std::cout << '\n';
+void edgesToDot(const edgesStack& edges, const std::string& filename) {
+    std::ofstream dotFile(filename);
+    if (!dotFile.is_open()) {
+        return;
     }
-}
-
-edgesStack getEdgesStack(matrix& M) {
-    edgesStack edges;
-    for (size_t i = 0; i < M.size(); ++i) {
-        for (size_t j = i + 1; j < M.size(); ++j) {
-            if(M[i][j]) {
-                edges.push_back({{i, j}, M[i][j]});
-            }
-        }
+    dotFile << "graph G {\n";
+    dotFile << "    node [shape=circle];\n\n";
+    dotFile << "\n";
+    for (const auto& e : edges) {
+        dotFile << "    "
+                << e.vertexes.first << " -- "
+                << e.vertexes.second
+                << " [label=\"" << e.weight
+                << "\" color=\"red\" penwidth=\"3\"];\n";
     }
-    return edges;
-}
-
-bool isGraphComplete(matrix& M, edgesStack& edges) {
-    return (M.size() * (M.size() - 1)) / 2 == edges.size();
+    dotFile << "}\n";
 }
 
 bool isMatrixSimmetric(matrix& M) {
     for (size_t i = 0; i < M.size(); ++i) {
         for (size_t j = i + 1; j < M.size(); ++j) {
-            if(M[i][j] != M[j][i]) {
+            if (M[i][j] != M[j][i]) {
                 return false;
             }
         }
@@ -95,92 +102,132 @@ bool isMatrixSimmetric(matrix& M) {
     return true;
 }
 
-bool isEdgesLooped(edgesStack& edges, int numVertices) {
-    if (edges.empty()) {
-        return false;
-    }
-    std::vector<int> parent(numVertices);
-    for (int i = 0; i < numVertices; ++i) {
-        parent[i] = i;
-    }
-    for (const auto& e : edges) {
-        int root1 = e.vertexes.first;
-        while (parent[root1] != root1) {
-            root1 = parent[root1];
+edgesStack getEdgesStack(matrix& M) {
+    edgesStack edges;
+    for (size_t i = 0; i < M.size(); ++i) {
+        for (size_t j = i + 1; j < M.size(); ++j) {
+            if (M[i][j]) {
+                edges.push_back({{{(int)i}, {(int)j}}, M[i][j]});
+            }
         }
-        int root2 = e.vertexes.second;
-        while (parent[root2] != root2) {
-            root2 = parent[root2];
-        }
-        if (root1 == root2) {
-            return true;
-        }
-        parent[root1] = root2;
     }
-    return false;
+    return edges;
 }
 
-int kruskalAlgorithm(const edgesStack& edges, int numVertices, bool degLimit) {
-    edgesStack result;
-    std::vector<int> degree(numVertices, 0);
+int kruskalLowerBound(const edgesStack& edges, int n) {
+    DSU dsu(n);
+    int weight = 0;
+    int cnt = 0;
+
     for (const auto& e : edges) {
         int u = e.vertexes.first;
         int v = e.vertexes.second;
-        if (degLimit) {
-            if (degree[u] >= MAX_VERTEX_DEG || degree[v] >= MAX_VERTEX_DEG) {
-                continue;
-            }
+
+        if (dsu.unite(u, v)) {
+            weight += e.weight;
+            cnt++;
+            if (cnt == n - 1) break;
         }
-        result.push_back(e);
-        if (!isEdgesLooped(result, numVertices)) {
-            if (degLimit) {
-                degree[u]++;
-                degree[v]++;
-            }
-        } else {
-            result.pop_back();
-        }
-        if (result.size() == numVertices - 1) break;
     }
-    if (result.size() != numVertices - 1) return 0;
-    int totalWeight = 0;
-    for (const auto& e : result) {
-        totalWeight += e.weight;
-    }
-    return totalWeight;
+
+    if (cnt != n - 1) return std::numeric_limits<int>::max();
+    return weight;
 }
 
-void findMSTRecursive(partialSolution& recursiveInfo) {
-    int MSTcostInAvailableEdges = kruskalAlgorithm(
-        recursiveInfo.sortedAvailableEdges,
-        recursiveInfo.vertexNumber,
-        false);
-    int currentMSTcostMinimum = recursiveInfo.currentResult + MSTcostInAvailableEdges;
-    if (currentMSTcostMinimum >= recursiveInfo.bestResult) {
+void findMSTRecursive(
+    edgesStack& edges,
+    int index,
+    std::vector<int>& degree,
+    DSU dsu,
+    int currentWeight,
+    int& bestWeight,
+    edgesStack& currentEdges,
+    edgesStack& bestEdges,
+    int n
+) {
+    if (currentEdges.size() == n - 1) {
+        if (currentWeight < bestWeight) {
+            bestWeight = currentWeight;
+            bestEdges = currentEdges;
+        }
         return;
     }
-    //
+    if (index >= edges.size()) {
+        return;
+    }
+    edgesStack remaining(edges.begin() + index, edges.end());
+    int bound = kruskalLowerBound(remaining, n);
+    if (bound == std::numeric_limits<int>::max()) {
+        return;
+    }
+    if (currentWeight + bound >= bestWeight) {
+        return;
+    }
+    auto& e = edges[index];
+    int u = e.vertexes.first;
+    int v = e.vertexes.second;
+    if (degree[u] < MAX_VERTEX_DEG && degree[v] < MAX_VERTEX_DEG) {
+        DSU dsuCopy = dsu;
+        if (dsuCopy.unite(u, v)) {
+            degree[u]++;
+            degree[v]++;
+            currentEdges.push_back(e);
+            findMSTRecursive(
+                edges, index + 1,
+                degree, dsuCopy,
+                currentWeight + e.weight,
+                bestWeight,
+                currentEdges, bestEdges,
+                n
+            );
+            currentEdges.pop_back();
+            degree[u]--;
+            degree[v]--;
+        }
+    }
+    findMSTRecursive(
+        edges, index + 1,
+        degree, dsu,
+        currentWeight,
+        bestWeight,
+        currentEdges, bestEdges,
+        n
+    );
 }
 
 int findMSTBranchAndBound(int maxVertexDeg) {
     matrix M = getMatrixFromFile();
+    matrixToDot(M, GRAPH_FILENAME);
     if (!isMatrixSimmetric(M)) {
         return EXIT_FAILURE;
     }
     edgesStack edges = getEdgesStack(M);
-    // std::cout << '\n';
-    // std::cout << isGraphComplete(M, edges);
-    // std::cout << '\n';
-    // std::cout << isMatrixSimmetric(M);
-    // std::cout << '\n';
-    // std::cout << isEdgesLooped(edges, M.size());
-    // std::cout << '\n';
-    // for (auto& edge : getEdgesStack(M)) {
-    //     std::cout << edge.vertexes.first << ' ' << edge.vertexes.second << ' ' << edge.weight << '\n';
-    // }
-    // printMatrix(M);
-    // matrixToDot(M, GRAPH_FILENAME);
-    // matrixToDot(M, MST_FILENAME);
+    std::sort(edges.begin(), edges.end(),
+        [](const edge& a, const edge& b) {
+            return a.weight < b.weight;
+        });
+    int n = M.size();
+    std::vector<int> degree(n, 0);
+    DSU dsu(n);
+    int bestWeight = std::numeric_limits<int>::max();
+    edgesStack currentEdges, bestEdges;
+    findMSTRecursive(
+        edges, 0,
+        degree, dsu,
+        0,
+        bestWeight,
+        currentEdges,
+        bestEdges,
+        n
+    );
+    std::cout << "BEST MST WEIGHT = " << bestWeight << '\n';
+    std::cout << "Edges:\n";
+    for (auto& e : bestEdges) {
+        std::cout << e.vertexes.first << " - "
+                  << e.vertexes.second
+                  << " (" << e.weight << ")\n";
+    }
+    edgesToDot(bestEdges, MST_FILENAME);
     return EXIT_SUCCESS;
 }
 
